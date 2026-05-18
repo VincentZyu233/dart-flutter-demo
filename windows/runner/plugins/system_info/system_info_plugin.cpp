cea2e8770007e8fc3c9ff4765b818ac0abe771b1
VincentZyu233 <1830540513zyu@gmail.com>
2026-05-18 10:27:36 +0800

    fix(build): WINSOCK 头文件冲突 + WndProc 未限定类名导致 CI 编译失败   system_info_plugin.cpp 加 WIN32_LEAN_AND_MEAN，切除旧 winsock.h   win32_window.cpp WndProc → Win32Window::WndProc   版本号 0.3.6-rc.12 → 0.3.6-rc.13   build release

--- windows/runner/plugins/system_info/system_info_plugin.cpp
+++ windows/runner/plugins/system_info/system_info_plugin.cpp
@@ -3,6 +3,7 @@
 
 #include "system_info_plugin.h"
 
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winsock2.h>
 #include <iphlpapi.h>
--- windows/runner/win32_window.cpp
+++ windows/runner/win32_window.cpp
@@ -55,7 +55,7 @@ class WindowClassRegistrar {
           ::LoadIcon(window_class.hInstance, MAKEINTRESOURCE(IDI_APP_ICON));
       window_class.hbrBackground = 0;
       window_class.lpszMenuName = nullptr;
-      window_class.lpfnWndProc = WndProc;
+      window_class.lpfnWndProc = Win32Window::WndProc;
       registered_class_ = ::RegisterClass(&window_class);
       class_registered_ = true;
     }
